ddf3d15e84ffe556ba2de56b92271230c0eb1733
Shashi Bhushan Singh <shashibhushan3105@gmail.com>
2024-09-02 16:55:03 +0530

    blog

---
+++ 06Reactrouter/src/Components/GitHub/GitHub.jsx
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function GitHub() {
+    const data = useLoaderData()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GitHub
